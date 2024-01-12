82e2d26fc7128ee78f0eed9c573dbaac57d7fa75
YehuditSa <yehudit2003@gmail.com>
2024-01-12 12:58:07 +0200

    i Changed only photohs + pageOfPhotos.

--- src/pages/Home/Albums/PageOfPhotos.jsx
+++ src/pages/Home/Albums/PageOfPhotos.jsx
@@ -1,19 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import UpdateWindow from '../../../components/UpdateWindow'
 import AddWindow from '../../../components/AddWindow';
-import styles from '../../../css/Albums.module.css'
+import styles from'../../../css/Albums.module.css'
 
-function PageOfPhotos({ isAddPhotosWindowShow, setIsAddPhotosWindowShow, generalDataAndTools, albumId, currentPage, photosToShow, isFirst }) {
+function PageOfPhotos({ PHOTOS_IN_PAGE, isAddPhotoWindowShow, setIsAddPhotoWindowShow, generalDataAndTools, albumId, currentPage, photosToShow}) {
 
     const [photos, setPhotos] = useState(photosToShow.flat());
     const [isGotPhotos, setIsGotPhotos] = useState(false);
     const [currentUpdated, setCurrentUpdated] = useState(null);
-    const LIMIT = 5;
 
     useEffect(() => {
         (async () => {
             if (photos.length == 0)
-                await generalDataAndTools.getItemsFunc(`albums/${albumId}/photos?_start=${(currentPage - 1) * 5}&_limit=${LIMIT}`, setPhotos, setIsGotPhotos)
+                await generalDataAndTools.getItemsFunc(`albums/${albumId}/photos?_start=${(currentPage - 1) * PHOTOS_IN_PAGE}&_limit=${PHOTOS_IN_PAGE}`, setPhotos, setIsGotPhotos)
             else
                 setIsGotPhotos(true);
         })();
@@ -35,8 +34,8 @@ function PageOfPhotos({ isAddPhotosWindowShow, setIsAddPhotosWindowShow, general
     }
     return (
         <>
-            {isAddPhotosWindowShow &&
-                <AddWindow setIsAddWindowShow={setIsAddPhotosWindowShow} baseItem={{
+            {isAddPhotoWindowShow &&
+                <AddWindow setIsAddWindowShow={setIsAddPhotoWindowShow} baseItem={{
                     albumId: albumId,
                     title: '',
                     url: '',
@@ -51,7 +50,7 @@ function PageOfPhotos({ isAddPhotosWindowShow, setIsAddPhotosWindowShow, general
             {!isGotPhotos && <p>Loading...</p>}
             {isGotPhotos && <div className={styles.photos}>
                 {photos.map((photo) => {
-                    return <div id={photo.id} className={styles.photo}>
+                    return <div key={photo.id} id={photo.id} className={styles.photo}>
                         <p>{photo.id}</p>
                         <p className={styles.photoTitle}>{photo.title}</p>
                         <img src={photo.thumbnailUrl} /><br />
--- src/pages/Home/Albums/Photos.jsx
+++ src/pages/Home/Albums/Photos.jsx
@@ -1,33 +1,86 @@
-import React from 'react'
-import { useState } from 'react'
-import { useOutletContext, useParams} from 'react-router-dom';
-import PageOfPhotos from './PageOfPhotos';
-import styles from'../../../css/Albums.module.css'
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { useOutletContext, useParams } from "react-router-dom";
+import PageOfPhotos from "./PageOfPhotos";
+import styles from "../../../css/Albums.module.css";
 
 function Photos() {
-    const [numOfPages, setNumOfPages] = useState(1);
-    const { albumId } = useParams();
-    const [pageNames, setPageNames] = useState([`album${albumId}page1`]);
-    const [isFirst,setIsFirst]=useState(true);
-    const [isAddPhotosWindowShow, setIsAddPhotosWindowShow] = useState(false);
-    const generalDataAndTools = useOutletContext();
-    return (
-        <>
-            <button className={styles.addButton}  onClick={() =>  setIsAddPhotosWindowShow(true)}>➕</button>
-            {pageNames.map((name => {
-                return<div > <PageOfPhotos isAddPhotosWindowShow={isAddPhotosWindowShow} setIsAddPhotosWindowShow={setIsAddPhotosWindowShow} generalDataAndTools={generalDataAndTools} albumId={albumId} currentPage={numOfPages} photosToShow={localStorage.getItem(name) == undefined ? [] : [JSON.parse(localStorage.getItem(name))] } isFirst={isFirst}/></div>
-            }))}
-            <button onClick={() => {
-                setIsFirst(false);
-                setPageNames((prev) => {
-                    setNumOfPages(prev => prev + 1)
-                    const tempPageName = [...prev];
-                    tempPageName.push(`album${albumId}page${numOfPages + 1}`)
-                    return tempPageName;
-                })
-            }}>show more</button>
-        </>
-    )
+
+  const [currentPages, setCurrentPages] = useState([1, 2]);
+  const [isAddPhotoWindowShow, setIsAddPhotoWindowShow] = useState(false);
+  const [isShowMoreOption, setIsShowMoreOption] = useState(true);
+  const [sumOfPhotosInAlbum, setSumOfPhotosInAlbum] = useState(null);
+  const { albumId } = useParams();
+  const generalDataAndTools = useOutletContext();
+
+  const PHOTOS_IN_PAGE = 5;
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(`http://localhost:3000/albums/${albumId}/photos/?_start=0&_limit=0`);
+      setSumOfPhotosInAlbum(parseInt(response.headers.get("X-Total-Count")));
+    })();
+  }, []);
+
+  useEffect(() => {
+    setIsShowMoreOption(sumOfPhotosInAlbum / PHOTOS_IN_PAGE > currentPages.at(-1));
+  }, [sumOfPhotosInAlbum, currentPages]);
+
+
+//  useEffect(() => {
+//   const handleScroll = () => {
+//     if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+//       setCurrentPages((prev) => {
+//         const tempCurrentPages = [...prev];
+//         tempCurrentPages.push(currentPages.at(-1) + 1);
+//         return tempCurrentPages;
+//       })
+//     }
+//   };
+
+//   window.addEventListener("scroll", handleScroll);
+
+//   return () => {
+//     window.removeEventListener("scroll", handleScroll);
+//   };
+// }, []);
+  
+  
+  return (
+    <>
+      <button className={styles.addButton} onClick={() => setIsAddPhotoWindowShow(true)}>
+        ➕
+      </button>
+      {currentPages.map((name) => {
+        return (
+          <div key={name}>
+            <PageOfPhotos
+              PHOTOS_IN_PAGE = {PHOTOS_IN_PAGE}
+              isAddPhotoWindowShow={isAddPhotoWindowShow}
+              setIsAddPhotoWindowShow={setIsAddPhotoWindowShow}
+              generalDataAndTools={generalDataAndTools}
+              albumId={albumId}
+              currentPage={name}
+              photosToShow={localStorage.getItem(name) == undefined ? [] : [JSON.parse(localStorage.getItem(name))]}
+            />
+          </div>
+        );
+      })}
+      {isShowMoreOption && (
+        <button
+          onClick={() => {
+            setCurrentPages((prev) => {
+              const tempCurrentPages = [...prev];
+              tempCurrentPages.push(currentPages.at(-1) + 1);
+              return tempCurrentPages;
+            });
+          }}
+        >
+          show more
+        </button>
+      )}
+    </>
+  );
 }
 
-export default Photos
+export default Photos;
